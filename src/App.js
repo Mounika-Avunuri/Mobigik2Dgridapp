40ca7a54b24b41d75c9f9e2f3526a14c40ddd8e8
mounika <mounikaaileni7@gmail.com>
2022-03-25 21:35:29 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState, useEffect } from 'react';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>Welcome </h4>
+      <input style={{ width: "90%", padding: "5px", margin: "10px", textalign: "center", outline: "none", borderradius: "10px", fontsize: "14px", fontweight: "600" }}
+        type="number" placeholder='Enter the number of rows' />
+      <input style={{ width: "90%", padding: "5px", margin: "10px", textalign: "center", outline: "none", borderradius: "10px", fontsize: "14px", fontweight: "600" }} type="number" placeholder='Enter the number of columns' />
+      <input type="text" style={{ display: "inline-block", margin: "20px 0" }} placeholder='search result' />
+      <button style={{ borderradius: "8px", textalign: "center", padding: "8px 3px" }}>search</button>
+      <button style={{ width: "100px", padding: "8px 3px", fontweight: "bold", borderradius: "10px", float: "right", margin: "20px 0" }}>create</button>
+
     </div>
+
   );
 }
 
